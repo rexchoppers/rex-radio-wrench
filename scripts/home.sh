bee6475869defda39ed07c4dc1d811d9e04be69c
rexchoppers <rexchoppers@gmail.com>
2025-10-28 20:08:21 +0000

    WIP Adding features for editing

--- scripts/home.sh
+++ scripts/home.sh
@@ -4,12 +4,10 @@ home() {
   choice=$(dialog --clear --stdout \
     --title "Rex Radio Wrench" \
     --menu "Choose an action:" 15 50 5 \
-    1 "Update Radio Information" \
-    2 "About" \
-    3 "Exit")
+    1 "Update Radio Information")
 
   case $choice in
-    1) update ;;
+    1) update_radio ;;
     2) show_versions ;;
     3) clear; exit ;;
   esac
--- scripts/update_radio.sh
+++ scripts/update_radio.sh
@@ -1,13 +1,19 @@
 update_radio() {
-  choice=$(dialog --clear --stdout \
-    --title "Update Radio Information" \
-    --menu "Choose a parameter to update:" 15 50 5 \
-    1 "Radio Name" \
-    2 "Back" \
-    3 "Exit")
+    choice=$(dialog --clear --stdout \
+        --title "Update Radio Information" \
+        --menu "Choose a parameter to update:" 15 50 5 \
+        1 "Radio Name"
+    )
 
-  case $choice in
-    1) update_radio_name ;;
-    2) clear; exit ;;
-  esac
+    if [ -z "$choice" ]; then
+        home
+        return
+    fi
+
+    case $choice in
+        1) update_radio_name ;;
+    esac
+
+    home
 }
+
